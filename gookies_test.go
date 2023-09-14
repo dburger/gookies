939081a4285237d9e78bd452ec2d4100c6013ac6
David J. Burger <davidburger@gmail.com>
2023-09-14 11:37:40 -0500

    Add simple tests
    Add some simple tests. These tests attempt to connect
    to a chrome instance running in debug mode on the specified
    port.

--- gookies_test.go
+++ gookies_test.go
@@ -1,10 +1,32 @@
 package gookies
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestGetCookies(t *testing.T) {
-	// TODO(dburger): test after made non OJ specific.
-	// cookies, err := GetOddsJamCookies("http://127.0.0.1:9222")
+// These tests require chrome to be running in debug mode on the port as shown below.
+const hostport = "http://127.0.0.1:9222"
+const invalidHostport = "http://127.0.0.1:1222"
+const website = "google.com"
+const invalidWebsite = "xgrooglex.com"
+
+func TestGetCookies_Works(t *testing.T) {
+	if cookies, err := GetCookies(hostport, website); err != nil {
+		assert.Nil(t, err, "Is you browser running in debug mode @ %v with a \"%v\" tab open?", hostport, website)
+	} else {
+		assert.Greater(t, len(cookies), 0)
+	}
+}
+
+func TestGetCookies_FailsNoChrome(t *testing.T) {
+	cookies, err := GetCookies(invalidHostport, website)
+	assert.NotNil(t, err)
+	assert.Emptyf(t, cookies, "Expected cookies to be empty when an error is returned.")
+}
+
+func TestGetCookies_FailsNoWebsite(t *testing.T) {
+	cookies, err := GetCookies(hostport, invalidWebsite)
+	assert.NotNil(t, err)
+	assert.Emptyf(t, cookies, "Expected cookies to be empty when an error is returned.")
 }
